a1d9506b1b9dd8ea14e2f487ff010e9a14c50b5f
2018ik <kang43@gmail.com>
2024-02-15 08:54:38 -0800

    fix typo

--- display.php
+++ display.php
@@ -23,7 +23,7 @@
               console.log(xhr);
               if (xhr.status === 200) {
                 var hymnNumbers = xhr.responseText;
-                if (hymnNumbers === null) {
+                if (hymnNumbers === "null") {
                   document.getElementById("engKorean").innerText = '-';
                   document.getElementById("chinese").innerText = '-';
                   document.getElementById("spanish").innerText = '-';
